4e459c3d68af449f8adf209d61f336cfe6c802bc
Marek <m.necesany@choosit.com>
2022-04-26 17:03:26 +0200

    custom attribute addition

---
+++ src/Attribute/IconAttributeType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Attribute;
+
+use Sylius\Component\Attribute\AttributeType\AttributeTypeInterface;
+use Sylius\Component\Attribute\Model\AttributeValueInterface;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+
+class IconAttributeType implements AttributeTypeInterface
+{
+    public const TYPE_ICON = 'icon';
+
+    public function getStorageType(): string
+    {
+        return AttributeValueInterface::STORAGE_JSON;
+    }
+
+    public function getType(): string
+    {
+        return self::TYPE_ICON;
+    }
+
+    public function validate(
+        AttributeValueInterface $attributeValue,
+        ExecutionContextInterface $context,
+        array $configuration
+    ): void {
+    }
+}
---
+++ src/Form/Type/AttributeTypeType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AttributeTypeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('icon');
+        $builder->add('text');
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setRequired('configuration');
+        $resolver->setDefined('locale_code');
+    }
+}
